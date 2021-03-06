eac3ce0c50f08f462ea15d5a9dfbfa27d5c35ff3
anstss <anastasiya31017@gmail.com>
2021-03-06 14:29:31 +0200

    feat: add solution for transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Array.isArray(arr) === false) {
+    throw new Error();
+  } else {
+    let resultArr = arr.slice(0);
+    const discardNext = "--discard-next";
+    const discardPrev = "--discard-prev";
+    const doubleNext = "--double-next";
+    const doublePrev = "--double-prev";
+    
+    for (let i = 0; i < resultArr.length; i++) {
+      let current = resultArr[i];
+      if (current === discardNext) {
+        resultArr.splice(i, 2, undefined);
+        i--;
+      } else if (current === discardPrev) {
+        resultArr[i - 1] === undefined ? resultArr.splice(i, 1) : resultArr.splice(i - 1, 2);
+        i--;
+      } else if (current === doubleNext) {
+        resultArr[i + 1] === undefined ? resultArr.splice(i, 1) : resultArr.splice(i, 1, resultArr[i + 1]);
+      } else if (current === doublePrev) {
+        resultArr[i - 1] === undefined ? resultArr.splice(i, 1) : resultArr.splice(i, 1, resultArr[i - 1]);
+      }
+    }
+
+    for (let i = 0; i < resultArr.length; i++) {
+      if (resultArr[i] === undefined) {
+        resultArr.splice(i, 1);
+        i--;
+      }
+    }
+
+    return resultArr;
+  }
 };
