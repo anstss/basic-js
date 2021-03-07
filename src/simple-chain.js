cb6f3321a9bb0f5c49e0daaf758950f17d000ed1
anstss <anastasiya31017@gmail.com>
2021-03-08 00:24:49 +0200

    feat: add solution for simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,24 +2,46 @@ const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.length = length;
+    return this;
   },
+
+  chainElements: [],
+  chain: ``,
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.value = value;
+    if (this.chainElements.length === 0) {
+      this.position = 1;
+      this.length = 1;
+    } else {
+      this.position++;
+      this.length++;
+    }
+    this.chainElements.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.position = position;
+    if (Number.isNaN(position) === true || Number.isInteger(position) === false) {
+      this.chainElements = [];
+      throw new Error();
+    }
+    this.chainElements.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chainElements = this.chainElements.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chainElements.forEach(function (elem, index) {
+      let tmp = ``;
+      elem === undefined ? tmp = `(  )` : tmp = `( ${elem} )`;
+      index === 0 ? chain = tmp : chain += `~~${tmp}`;
+    });
+    this.chainElements = [];
+    return chain;
   }
 };
 
